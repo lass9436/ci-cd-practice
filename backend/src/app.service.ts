5f138aa51a2de277a015de34830c06cd9316e0cf
lass9436 <lass9436@gmail.com>
2022-11-17 15:42:30 +0900

    cd pull backend test

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! this is version 3';
+    return 'Hello World! this is version 4. \ndo you pull only backend directory?';
   }
 }
